b93f6ecad9552d235729d0321856d37f5c58299e
Galina Chzhen <g.chzhen@MyLocalMachine.local>
2018-12-12 19:33:43 -0800

    first 10 are solved

--- src/recursion.js
+++ src/recursion.js
@@ -7,31 +7,89 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+	if (n < 0) {
+		return null
+	} else if (n === 0) {
+		return 1
+	};
+	return (n * factorial(n - 1));
 };
 
 // 2. Compute the sum of an array of integers.
-// sum([1,2,3,4,5,6]); // 21
+ //sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+	if (array.length === 0) {return 0};
+	if (array.length === 1) {
+		return array[0];
+	};
+	return array[0] + sum(array.slice(1))
 };
+//(1 + (2 + (3 + f([4]))))
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+	var total = 0;
+	// declare currentValue
+	for (var i = 0; i < array.length; i++) { 
+	  var currentValue = array[i]; 
+	  if (typeof(currentValue) === 'number'){
+	  	total += currentValue // if a number adds to total 
+	  };
+	  if (Array.isArray(currentValue))
+		total += arraySum(currentValue); // if an array goes inside and adds to total
+	}; 
+	return total;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+	if (n === 0) {
+		return true;
+	} else if (n === 1 || n === -1) {
+		return false;
+	} else {
+	  if (n > 1) {
+	  	return isEven(n - 2);
+	  } else if (n < -1) {
+		return isEven(n + 2);
+	  }
+	}
 };
-
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
-// sumBelow(7); // 21
+// sumBelow(7); // 21 
 var sumBelow = function(n) {
-};
+	var total = 0;
+  if (n === 0) {
+  	return 0;
+  } else if (n > 0) {
+  	total += n - 1;
+    total += sumBelow(n - 1);
+  } else if (n < 0) {
+  	total += n + 1;
+  	total += sumBelow(n + 1);
+  };
+  return total;
+}; 
 
 // 6. Get the integers within a range (x, y).
-// range(2,9); // [3,4,5,6,7,8]
-var range = function(x, y) {
+// range(3,8); // [4,5,6,7]
+var range = function(x, y) { 
+	var numsWithinRange = [];
+	var increaseX = x + 1;
+	var decreaseX = x - 1;
+	if (x === y){
+		return numsWithinRange;
+	};
+	if (increaseX < y )  {
+	  numsWithinRange.push(increaseX);
+	  return numsWithinRange.concat(range(increaseX, y));
+	} else if (decreaseX > y )  {
+	  numsWithinRange.push(decreaseX);
+	  return numsWithinRange.concat(range(decreaseX, y));
+	};
+	return numsWithinRange;
 };
 
 // 7. Compute the exponent of a number.
@@ -39,23 +97,55 @@ var range = function(x, y) {
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
-var exponent = function(base, exp) {
+var exponent = function(base, exp) { 
+	if (exp === 0) {	return 1; }
+	 else {
+		if (exp < 0) { 
+			return 1/exponent(base, exp*(-1)); 
+		} else if (exp > 0) {
+		    return base * exponent(base, exp - 1);
+		};
+	}
 };
 
 // 8. Determine if a number is a power of two.
-// powerOfTwo(1); // true
-// powerOfTwo(16); // true
-// powerOfTwo(10); // false
+// powerOfTwo(1); // true 2^0 = 1
+// powerOfTwo(16); // true 2^4 = 16 // 
+// powerOfTwo(10); // false  // 
 var powerOfTwo = function(n) {
+	if (n === 1) {return true};
+	if (n === 2) {
+		return true;
+	} else if ( n > 2) {
+		return powerOfTwo(n/2);
+	} else {
+		return false;
+	}
 };
 
 // 9. Write a function that reverses a string.
-var reverse = function(string) {
+var reverse = function(string) { 
+	if (string === '') {
+		return ''
+	} else {
+		return reverse(string.slice(1)) + string[0] 
+	};
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+	var lastChar = string.length -1
+	if (string.length === 1 || string.length === 0) {
+		return true;
+	};
+
+	if (string[0].toLowerCase() === string[lastChar].toLowerCase()){
+		return palindrome(string.slice(1, lastChar)) 
+	} else {
+		return false;
+	};
 };
+
 
 // 11. Write a function that returns the remainder of x divided by y without using the
 // modulo (%) operator.
